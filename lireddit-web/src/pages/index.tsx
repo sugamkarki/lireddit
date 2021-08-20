5b2186c49b50dab32984e327f7e03b11d23b919a
sugam <sugamkarki7058@gmail.com>
2021-08-20 19:21:46 +0545

    basic layout setup done

---
+++ lireddit-web/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+
+const Index = () => <div className="">Hello world</div>;
+
+export default Index;
