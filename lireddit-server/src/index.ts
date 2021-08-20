833ae40758ea6b7b4633dcdb0bc0571c432604a1
sugam <sugamkarki7058@gmail.com>
2021-08-20 09:46:18 +0545

    mikro done for now

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,14 +1,20 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "apollo-server-express";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  //   const post = orm.em.create(Post, { title: "my first post" });
-  //   await orm.em.persistAndFlush(post);
-  //   const posts = await orm.em.find(Post, {});
-  //   console.log(posts);
+  const app = express();
+    const apolloServer=new ApolloServer({
+        schema;
+    })
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 main().catch((err) => {
   console.log(err);
